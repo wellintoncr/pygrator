c836b7766c312dbff312edac7b6041445ef673ed
wellintoncr <wcostaee@gmail.com>
2022-07-30 19:21:09 -0300

    Update table to add or remove columns

--- src/migrator.py
+++ src/migrator.py
@@ -21,6 +21,10 @@ class Migrator:
             if pos == 0:
                 script = await self.create_table(each_migration["model"], table_name)
                 output.append(script)
+            else:
+                script = await self.update_table(
+                    content=content, position=pos, table_name=table_name)
+                output.append(script)
         return output
 
     async def create_table(self, migration_data, table_name):
@@ -31,3 +35,38 @@ class Migrator:
             columns.append(f"{column} {field.field_type.to_database()}")
         script = script.format(table_name=table_name, columns=",".join(columns))
         return script
+
+    async def update_table(self, content: list, position: int, table_name: str):
+        delta = await self.calculate_delta(
+            content[position - 1]["model"], content[position]["model"])
+        script_to_add = ""
+        script_to_delete = ""
+        if delta["add"]:
+            columns = []
+            for column, data in delta["add"].items():
+                field = await Field.from_str(data)
+                columns.append(f"{column} {field.field_type.to_database()}")
+            columns.sort(key=lambda item: item)
+            script_to_add = "ALTER TABLE {table_name} ADD {columns};"
+            script_to_add = script_to_add.format(table_name=table_name, columns=",".join(columns))
+        if delta["remove"]:
+            columns = list(delta["remove"].keys())
+            columns.sort(key=lambda item: item)
+            script_to_add = "ALTER TABLE {table_name} DROP COLUMN {columns};"
+            script_to_add = script_to_add.format(table_name=table_name, columns=",".join(columns))
+        return script_to_add + script_to_delete
+
+    async def calculate_delta(self, old_model, new_model):
+        old_fields = set(old_model.keys())
+        new_fields = set(new_model.keys())
+        output = {
+            "add": {},
+            "remove": {}
+        }
+        for field in new_fields:
+            if field not in old_fields:
+                output["add"][field] = new_model[field]
+        for field in old_fields:
+            if field not in new_fields:
+                output["remove"][field] = old_model[field]
+        return output
--- src/tests/test_migrator.py
+++ src/tests/test_migrator.py
@@ -1,5 +1,8 @@
+
 import json
 import tempfile
+from unittest import mock
+
 
 from migrator import Migrator
 
@@ -7,7 +10,7 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_create_script():
+async def test_create_script_do_nothing():
     migrator = Migrator()
     migration_file = tempfile.NamedTemporaryFile()
 
@@ -27,6 +30,12 @@ async def test_create_script():
         table_name="new_table"
     )
     assert response is None
+
+
+@pytest.mark.asyncio
+async def test_create_script_create_table():
+    migrator = Migrator()
+    migration_file = tempfile.NamedTemporaryFile()
 
     # Migration file has one entry -> should create table
     content = [
@@ -48,4 +57,166 @@ async def test_create_script():
         table_name="new_table"
     )
     expected = ["CREATE TABLE new_table (id INT,name VARCHAR(255))"]
+    assert response == expected
+
+
+@pytest.mark.asyncio
+async def test_create_script_add_column():
+    migrator = Migrator()
+    migration_file = tempfile.NamedTemporaryFile()
+
+    # Migration file has two entries -> should create table and add a column
+    content = [
+        {
+            "model": {
+                "id": {
+                    "field_type": "<type Integer>"
+                },
+                "name": {
+                    "field_type": "<type String>"
+                }
+            }
+        },
+        {
+            "model": {
+                "id": {
+                    "field_type": "<type Integer>"
+                },
+                "name": {
+                    "field_type": "<type String>"
+                },
+                "amount": {
+                    "field_type": "<type Float>"
+                },
+                "surname": {
+                    "field_type": "<type String>"
+                }
+            }
+        }
+    ]
+    migration_file.write(bytes(json.dumps(content), 'utf-8'))
+    migration_file.seek(0)
+    with mock.patch("migrator.Migrator.create_table") as create_table_mock:
+        create_table_mock.return_value = "mock"
+        response = await migrator.create_script(
+            migration_file=migration_file.name,
+            table_name="new_table"
+        )
+        expected = "ALTER TABLE new_table ADD amount DECIMAL(10,2),surname VARCHAR(255);"
+        assert response[1] == expected
+
+
+@pytest.mark.asyncio
+async def test_create_script_remove_column():
+    migrator = Migrator()
+    migration_file = tempfile.NamedTemporaryFile()
+
+    # Migration file has two entries -> should create table and remove a column
+    content = [
+        {
+            "model": {
+                "id": {
+                    "field_type": "<type Integer>"
+                },
+                "name": {
+                    "field_type": "<type String>"
+                },
+                "amount": {
+                    "field_type": "<type Float>"
+                },
+                "surname": {
+                    "field_type": "<type String>"
+                }
+            }
+        },
+        {
+            "model": {
+                "id": {
+                    "field_type": "<type Integer>"
+                },
+                "name": {
+                    "field_type": "<type String>"
+                }
+            }
+        }
+    ]
+    migration_file.write(bytes(json.dumps(content), 'utf-8'))
+    migration_file.seek(0)
+    with mock.patch("migrator.Migrator.create_table") as create_table_mock:
+        create_table_mock.return_value = "mock"
+        response = await migrator.create_script(
+            migration_file=migration_file.name,
+            table_name="new_table"
+        )
+        expected = "ALTER TABLE new_table DROP COLUMN amount,surname;"
+        assert response[1] == expected
+
+
+@pytest.mark.asyncio
+async def test_calculate_delta_new_fields():
+    migrator = Migrator()
+    # New fields
+    old_model = {
+        "id": {
+            "field_type": "<type Integer>"
+        }
+    }
+    new_model = {
+        "id": {
+            "field_type": "<type Integer>"
+        },
+        "name": {
+            "field_type": "<type String>"
+        },
+        "amount": {
+            "field_type": "<type Float>"
+        }
+    }
+    response = await migrator.calculate_delta(old_model, new_model)
+    expected = {
+        "add": {
+            "name": {
+                "field_type": "<type String>"
+            },
+            "amount": {
+                "field_type": "<type Float>"
+            }
+        },
+        "remove": {}
+    }
+    assert response == expected
+
+
+@pytest.mark.asyncio
+async def test_calculate_delta_delete_fields():
+    migrator = Migrator()
+    # New fields
+    old_model = {
+        "id": {
+            "field_type": "<type Integer>"
+        },
+        "name": {
+            "field_type": "<type String>"
+        },
+        "amount": {
+            "field_type": "<type Float>"
+        }
+    }
+    new_model = {
+        "id": {
+            "field_type": "<type Integer>"
+        }
+    }
+    response = await migrator.calculate_delta(old_model, new_model)
+    expected = {
+        "remove": {
+            "name": {
+                "field_type": "<type String>"
+            },
+            "amount": {
+                "field_type": "<type Float>"
+            }
+        },
+        "add": {}
+    }
     assert response == expected
